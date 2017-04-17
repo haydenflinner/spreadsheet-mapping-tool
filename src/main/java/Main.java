86e419744e260698fbb17f088ef0d653cd655c18
Hayden Flinner <hayden@gatech.edu>
2017-04-17 15:51:22 -0400

    added boilerplate

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,8 +1,21 @@
-/**
- * Created by hayden on 4/17/17.
- */
+import org.apache.poi.ss.usermodel.Cell;
+import org.apache.poi.ss.usermodel.Row;
+import org.apache.poi.ss.usermodel.Sheet;
+import org.apache.poi.ss.usermodel.Workbook;
+
+import java.io.FileInputStream;
+import java.io.InputStream;
+
 public class Main {
     public static void main(String...args) {
 
+        InputStream inp = new FileInputStream("workbook.xls");
+        //InputStream inp = new FileInputStream("workbook.xlsx");
+
+        Workbook wb = WorkbookFactory.create(inp);
+        Sheet sheet = wb.getSheetAt(0);
+        Row row = sheet.getRow(2);
+        Cell cell = row.getCell(3);
+        if (cell == null)
     }
 }
